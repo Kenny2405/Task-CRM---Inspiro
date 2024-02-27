a136d8570c4e54ca6a68a478db3918244fd47218
Christ-edmund23 <94790146+Christ-edmund23@users.noreply.github.com>
2024-02-27 09:50:57 +0700

    Add files via upload

---
+++ Task CRM/Task CRM/Models/Interaction.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_CRM.Models
+{
+    public class Interaction
+    {
+        public int Id { get; set; }
+        public string? Contact { get; set; }
+        public string? Client {  get; set; }
+        public string? JenisInteraksi { get; set; }
+        public DateOnly Tanggal { get; set; }
+        public TimeOnly Waktu {  get; set; }
+        public string? Detail {  get; set; }
+        public string? PIC_Sales { get; set; }
+        public string? status { get; set; }
+    }
+}
