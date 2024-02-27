be3d8904718ac8015b311c3c1e33606876d97c78
Kenny <108676829+Kenny2405@users.noreply.github.com>
2024-02-27 10:11:13 +0700

    Add files via upload

---
+++ Task CRM/Task CRM/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Task_CRM.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
